d2975223830ea3961556813043c3c7903a13dc4d
Bradley Owens <bradowens17@hotmail.com>
2022-06-20 17:32:21 +1000

    add basic styling of application

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
+const myKey = "AbHsodLTgCWRPCW4viifoUjHtdJVVmj1";
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function (location) {
-    console.log(location);
+    // console.log(location);
 
     const { latitude } = location.coords;
     const { longitude } = location.coords;
 
-    console.log(latitude, longitude);
+    // console.log(latitude, longitude);
 
     const currentLocation = [latitude, longitude];
 
     const map = L.map("map").setView(currentLocation, 13);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker(currentLocation)
-      .addTo(map)
-      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-      .openPopup();
+    L.marker(currentLocation).addTo(map).bindPopup("You are here").openPopup();
   });
 }
