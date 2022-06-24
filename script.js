d27b5efc2785e2244cde7e24522301c5d1a03842
Bradley Owens <bradowens17@hotmail.com>
2022-06-24 17:31:52 +1000

    add ccs style feature for seach bar when recieved directions have better view of map

--- script.js
+++ script.js
@@ -9,9 +9,8 @@ function initMap() {
 }
 window.initMap = initMap;
 
-// Get current location
+// Get current location and add marker
 navigator.geolocation.getCurrentPosition(function (location) {
-  console.log(location);
   let { latitude, longitude } = location.coords;
 
   let marker = new google.maps.Marker({
@@ -22,13 +21,20 @@ navigator.geolocation.getCurrentPosition(function (location) {
   });
 });
 
-// Get directions
+// Get directions//////////////////
 
 const from = document.getElementById("place-search-start");
 const to = document.getElementById("place-search-destination");
 const formOfTravel = document.getElementById("input-transit");
 const getDirectionsBtn = document.querySelector(".submit");
+const searchContainer = document.querySelector(".search-container");
 
+//return search bar to normal opacity
+searchContainer.addEventListener("click", () => {
+  document.querySelector(".user-container").style.opacity = 1;
+});
+
+//send request for directions
 getDirectionsBtn.addEventListener("click", function () {
   const chosenTravel = formOfTravel.value;
 
@@ -36,6 +42,7 @@ getDirectionsBtn.addEventListener("click", function () {
     origin: String(from.value),
     destination: String(to.value),
     travelMode: String(chosenTravel),
+    unitSystem: google.maps.UnitSystem.IMPERIAL,
   };
   let directionsService = new google.maps.DirectionsService();
   let directionsDisplay = new google.maps.DirectionsRenderer();
@@ -49,6 +56,8 @@ getDirectionsBtn.addEventListener("click", function () {
       directionsService.route(directionsRequest, function (result, status) {
         if (status == "OK") {
           directionsDisplay.setDirections(result);
+
+          document.querySelector(".user-container").style.opacity = 0.35;
         } else {
           alert("Failed to get directions! Check your inputted locations");
 
