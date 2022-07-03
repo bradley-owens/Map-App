c114fa6646b06af3678ce0dcce897a46d5162391
Bradley Owens <bradowens17@hotmail.com>
2022-07-03 12:59:27 +1000

    add feature to get directions from current locations to inputted destination

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ const getCurrentLocation = new Promise(function (resolve, reject) {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function showLocation(location) {
       let { latitude, longitude } = location.coords;
+
       let userLocation = [latitude, longitude];
 
       let marker = new google.maps.Marker({
@@ -51,8 +52,13 @@ const getCurrentLocation = new Promise(function (resolve, reject) {
   }
 });
 
-//User Location
-getCurrentLocation.then((res) => console.log(res));
+//Users Location
+// getCurrentLocation.then((res) => {
+//   const lat = res[0];
+//   const lon = res[1];
+
+//   console.log(lat, lon);
+// });
 
 ////////////////////////////////////////
 // Get directions//////////////////
@@ -70,56 +76,66 @@ document.getElementById("map").addEventListener("click", () => {
 
 //send request for directions
 getDirectionsBtn.addEventListener("click", function () {
-  const chosenTravel = formOfTravel.value;
+  getCurrentLocation.then((res) => {
+    const lat = res[0];
+    const lon = res[1];
 
-  let directionsRequest = {
-    origin: String(from.value),
-    destination: String(to.value),
-    travelMode: String(chosenTravel),
-  };
-  let directionsService = new google.maps.DirectionsService();
-  let directionsDisplay = new google.maps.DirectionsRenderer();
+    let currentLocation = `${lat}, ${lon}`;
+    const chosenTravel = formOfTravel.value;
 
-  directionsDisplay.setMap(map);
-
-  if (to === "" || from === "" || chosenTravel === "") {
-    alert("Please add your to and from with a mode of travel");
-  } else {
-    function calcRoute() {
-      directionsService.route(directionsRequest, function (result, status) {
-        if (status == "OK") {
-          // render route
-          directionsDisplay.setDirections(result);
-
-          // change opacity of search component once route is rendered
-          userContainer.style.opacity = 0.35;
-
-          // Distance and Duration of route
-
-          const output = document.getElementById("output");
-          const start = document.querySelector(".start");
-          const end = document.querySelector(".end");
-          const duration = document.querySelector(".duration");
-          const distance = document.querySelector(".distance");
-
-          // show output with information on route
-          output.classList.add("active");
-
-          start.textContent = `From: ${from.value}`;
-          end.textContent = `To: ${to.value}`;
-          distance.textContent = `Distance: ${result.routes[0].legs[0].distance.text}`;
-          duration.textContent = `Duration: ${result.routes[0].legs[0].duration.text}`;
-        } else {
-          alert("Failed to get directions! Check your inputted locations");
-
-          // reset input fields
-          to.value = "";
-          from.value = "";
-          formOfTravel.value = "";
-        }
-      });
+    if (from.value === "Current Location") {
+      from.value = currentLocation;
     }
+    let directionsRequest = {
+      origin: String(from.value),
+      destination: String(to.value),
+      travelMode: String(chosenTravel),
+    };
 
-    calcRoute();
-  }
+    let directionsService = new google.maps.DirectionsService();
+    let directionsDisplay = new google.maps.DirectionsRenderer();
+
+    directionsDisplay.setMap(map);
+
+    if (to === "" || from === "" || chosenTravel === "") {
+      alert("Please add your to and from with a mode of travel");
+    } else {
+      function calcRoute() {
+        directionsService.route(directionsRequest, function (result, status) {
+          if (status == "OK") {
+            // render route
+            directionsDisplay.setDirections(result);
+
+            // change opacity of search component once route is rendered
+            userContainer.style.opacity = 0.35;
+
+            // Distance and Duration of route
+
+            const output = document.getElementById("output");
+            const start = document.querySelector(".start");
+            const end = document.querySelector(".end");
+            const duration = document.querySelector(".duration");
+            const distance = document.querySelector(".distance");
+
+            // show output with information on route
+            output.classList.add("active");
+
+            start.textContent = `From: ${from.value}`;
+            end.textContent = `To: ${to.value}`;
+            distance.textContent = `Distance: ${result.routes[0].legs[0].distance.text}`;
+            duration.textContent = `Duration: ${result.routes[0].legs[0].duration.text}`;
+          } else {
+            alert("Failed to get directions! Check your inputted locations");
+
+            // reset input fields
+            to.value = "";
+            from.value = "";
+            formOfTravel.value = "";
+          }
+        });
+      }
+
+      calcRoute();
+    }
+  });
 });
